b0d3acdac1a0d78a2e4bbf52f6a152121a1576c2
Victor Henrique Dias <101878978+vhdtv@users.noreply.github.com>
2023-05-12 10:00:18 -0300

    Update OtimizacaoDeConsumoDeCombustivelResolucaoUm.java

--- test.java
+++ test.java
@@ -1,6 +1,5 @@
-package sintaxe_basica;
 import java.util.Scanner;
-public class test {
+public class OtimizacaoDeConsumoDeCombustivelResolucaoUm {
 	    public static void main(String[] args) {
 	        Scanner scan = new Scanner(System.in);
 	        System.out.print("Digite a placa do veículo: ");
